d722623614087ef54eaaac4489a066b48333965f
Kyal Lanum <kyallanum@gmail.com>
2026-03-24 14:01:01 -0600

    Adds moonraker config file. updates update.sh

--- update.sh
+++ update.sh
@@ -9,6 +9,6 @@ source /opt/config/mod/.shell/0.sh
 cp del_gcode.py ${MOD}/usr/data/config/base/moonraker/components/
 
 INC="[include plugins/del_gcode/moonraker.cfg]"
-FILE="/opt/config/mod_data/plugins.moonraker.conf"
+FILE="/opt/config/mod_data/plugins.moonraker.cfg"
 
 grep -qF "${INC}" "${FILE}" || echo "${INC}" >> "${FILE}"
