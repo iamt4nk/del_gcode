584e803114c885f947c67b59c8be5ecf32b3704a
Kyal Lanum <kyallanum@gmail.com>
2026-03-24 14:44:03 -0600

    updates update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ set -e
 
 source /opt/config/mod/.shell/0.sh
 
-cp del_gcode.py /opt/config/mod/.shell/
+ln -s del_gcode.py /opt/config/mod/.shell/del_gcode.py
 
 INC="[include plugins/del_gcode/moonraker.cfg]"
 FILE="/opt/config/mod_data/plugins.moonraker.cfg"
